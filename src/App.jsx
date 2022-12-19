9db72d97e525696c9b4e78fd97610f6521c2f7c0
Luis Herdeth <luis.herdeth@gmail.com>
2022-12-19 13:15:15 +0100

    Funciona la comparación y multiplica, falta cambiar el valor, por un input

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,58 @@ import { useEffect, useState } from 'react';
 
 function App() {
 	const [moneda, setMoneda] = useState([]);
-	const cantidad = 3;
-	let usd = 'AFN';
+	const [amountUser, setAmountUser] = useState(0);
 
-	const itemToFind = moneda.find((item) => item.to === usd);
-	console.log(itemToFind);
-	
+	const cantidad = (e) => {
+		setAmountUser(e.target.value);
+	};
+	const onFormSubmit = (e) => {
+		e.preventDefault();
+		const inputText = e.target[0].value;
+		const itemToFind = moneda.find((item) => item.to === inputText);
+		console.log(amountUser);
+		console.log(inputText);
+		itemToFind ? console.log(3 * itemToFind.amount) : 'error';
+	};
 	useEffect(() => {
 		fetchApi().then((getMoneda) => {
 			setMoneda(getMoneda);
 		});
 	}, []);
+	// const [amountUser, setAmountUser] = useState('')
+	// if (itemToFind) console.log(cantidad * itemToFind.amount);
 	return (
 		<div className="App">
-			{/* <form action="#">
+			<form action="#" onSubmit={onFormSubmit}>
 				<label htmlFor="#">Choose your divisas</label>
+				{/* <input type="number" value={amountUser} onChange={cantidad} /> */}
 				<input type="text" list="divisas" name="divisas" id="divisa" />
 				<datalist id="divisas">
 					<option value="USD"></option>
 					<option value="AED"></option>
 				</datalist>
-			</form> */}
-
-			{/* <div id="result"></div> */}
-
+			</form>
+			{/* <div id="result"></div>
 			{/* {moneda.find((item) => (item.to === usd ? 'bien' : 'mal'))} */}
 			{/* {moneda.find((item) => console.log(item.to))} */}
+			{/* <form action="#">
+				<label htmlFor="#">Choose your divisas</label>
+				<input
+					type="text"
+					value={amountUser}
+					onChange={(e) => {
+						setAmountUser(e.target.value);
+					}}
+					list="divisas"
+					name="divisas"
+					id="divisa"
+				/>
+				<datalist id="divisas">
+					<option value="USD"></option>
+					<option value="AED"></option>
+				</datalist> */}
+			{/* {itemToFind && <div>{cantidad * itemToFind.amount}</div>} */}
+			{/* </form> */}
 		</div>
 	);
 }
