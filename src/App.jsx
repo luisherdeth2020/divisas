9f7f8c2598e8c8bb87dff92a1ffbf17007b886ba
Luis Herdeth <luis.herdeth@gmail.com>
2022-12-19 14:28:13 +0100

    add map <option>, input fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,56 +6,38 @@ function App() {
 	const [moneda, setMoneda] = useState([]);
 	const [amountUser, setAmountUser] = useState(0);
 
-	const cantidad = (e) => {
+	const onInputChange = (e) => {
 		setAmountUser(e.target.value);
 	};
 	const onFormSubmit = (e) => {
 		e.preventDefault();
-		const inputText = e.target[0].value;
+		const inputText = e.target[1].value;
 		const itemToFind = moneda.find((item) => item.to === inputText);
-		console.log(amountUser);
-		console.log(inputText);
-		itemToFind ? console.log(3 * itemToFind.amount) : 'error';
+		console.log('valor de ' + amountUser);
+		console.log('valor datalist ' + inputText);
+		itemToFind ? console.log(amountUser * itemToFind.amount) : 'error';
 	};
 	useEffect(() => {
 		fetchApi().then((getMoneda) => {
 			setMoneda(getMoneda);
 		});
 	}, []);
-	// const [amountUser, setAmountUser] = useState('')
-	// if (itemToFind) console.log(cantidad * itemToFind.amount);
+
 	return (
 		<div className="App">
 			<form action="#" onSubmit={onFormSubmit}>
 				<label htmlFor="#">Choose your divisas</label>
-				{/* <input type="number" value={amountUser} onChange={cantidad} /> */}
-				<input type="text" list="divisas" name="divisas" id="divisa" />
-				<datalist id="divisas">
-					<option value="USD"></option>
-					<option value="AED"></option>
-				</datalist>
+				<input type="number" value={amountUser} onChange={onInputChange} />
+				<div>
+					<input type="text" list="currency" name="currency" id="divisa" />
+					<datalist id="currency">
+						{moneda.map((item, index) => (
+							<option key={index} value={item.to}></option>
+						))}
+					</datalist>
+				</div>
+				<input type="submit" />
 			</form>
-			{/* <div id="result"></div>
-			{/* {moneda.find((item) => (item.to === usd ? 'bien' : 'mal'))} */}
-			{/* {moneda.find((item) => console.log(item.to))} */}
-			{/* <form action="#">
-				<label htmlFor="#">Choose your divisas</label>
-				<input
-					type="text"
-					value={amountUser}
-					onChange={(e) => {
-						setAmountUser(e.target.value);
-					}}
-					list="divisas"
-					name="divisas"
-					id="divisa"
-				/>
-				<datalist id="divisas">
-					<option value="USD"></option>
-					<option value="AED"></option>
-				</datalist> */}
-			{/* {itemToFind && <div>{cantidad * itemToFind.amount}</div>} */}
-			{/* </form> */}
 		</div>
 	);
 }
